6130ca711c03412b69fed1573d321c809f215a30
Bence Imre Kiss <kissbenceimre99@gmail.com>
2020-06-11 11:30:13 +0200

    write in file

--- index.php
+++ index.php
@@ -3,18 +3,35 @@
 <body>
 
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "cego";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=cego", $username, $password);
-  // set the PDO error mode to exception
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  
+  $stmt = $conn->prepare("SELECT * FROM users");
+  $stmt->execute();
+
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  
+  $myfile = fopen("file.txt", "w");
+  
+  foreach($stmt->fetchAll() as $k => $v){
+	  foreach($v as $k => $a){
+		  $txt = $a . ",\t";
+		  fwrite($myfile, $txt);
+	  }
+	  fwrite($myfile, "\n");
+  }
+  
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  echo $e->getMessage();
 }
+
 ?> 
 
 </body>
